42c9926a6513fccc891b372328ab6d8a8348c50f
Douglas Dias <64196583+diasDouglascs@users.noreply.github.com>
2026-04-13 21:21:39 +0000

    adiciona scripts: lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório para aprender seguindo curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 function Home() {
-    return<h1>
-        Envolta do Mundo!
-    </h1>
+  return <h1>Envolta do Mundo!</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function test2() {
+  console.log("indentação errada");
 }
 
 export default Home;
